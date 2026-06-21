0a41a2ba7b789567b16223ba492d43aa939b7d3f
LZRcreeperReal <vexozvexoz@gmail.com>
2026-06-21 17:16:37 -0400

    Create sends.js

---
+++ sends.js
@@ -0,0 +1,33 @@
+const base = [
+  "did u get it",
+  "you get it",
+  "everything good",
+  "we good",
+  "you got it right",
+  "it came through yeah",
+  "all good",
+  "u received it",
+  "it work for u",
+  "you got it?"
+];
+
+const endings = ["", "", "", "?", " lol", " fr", ""];
+
+function pick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const sends = new Set();
+
+while (sends.size < 100) {
+  let msg = pick(base) + pick(endings);
+
+  // inconsistent typing like real Discord
+  if (Math.random() < 0.2) msg = msg.replace("you", "u");
+  if (Math.random() < 0.1) msg = msg.replace("are", "r");
+  if (Math.random() < 0.1) msg = msg.toLowerCase();
+
+  sends.add(msg);
+}
+
+module.exports = [...sends];
