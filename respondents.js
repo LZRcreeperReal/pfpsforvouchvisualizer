b65b1a25a79bee2ab982325051da916ea84c7aa1
LZRcreeperReal <vexozvexoz@gmail.com>
2026-06-21 17:17:09 -0400

    Create respondents.js

---
+++ respondents.js
@@ -0,0 +1,62 @@
+const phrases = [
+  "yeah ur good",
+  "its fine",
+  "all good here",
+  "that worked",
+  "yea i got it",
+  "i got it",
+  "looks good",
+  "no issues",
+  "seems fine",
+  "all set",
+  "thx again",
+  "appreciate it",
+  "nice",
+  "clean",
+  "perfect honestly",
+  "mb just saw this now but yeah",
+  "didnt have issues",
+  "yeah works",
+  "it came through",
+  "i see it now",
+  "yep got it",
+  "all good on my end",
+  "that’s fine",
+  "looks okay to me",
+  "yea thats good",
+  "all sorted",
+  "we good",
+  "its done",
+  "no problems"
+];
+
+const addOns = [
+  "",
+  "",
+  "",
+  " lol",
+  " fr",
+  " ngl",
+  " thanks",
+  " appreciate it",
+  " ok"
+];
+
+function pick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const responses = new Set();
+
+while (responses.size < 100) {
+  let msg = pick(phrases) + pick(addOns);
+
+  // random human messiness
+  if (Math.random() < 0.15) msg = msg.replace("good", "gud");
+  if (Math.random() < 0.1) msg = msg.replace("you", "u");
+  if (Math.random() < 0.1) msg += "...";
+
+  responses.add(msg);
+}
+
+module.exports = [...responses];
